fcc3435d60e69237a42bcd23c3733d8057453fda
GautamDhabhai <109916842+GautamDhabhai@users.noreply.github.com>
2023-09-29 11:41:21 +0530

    Day 2 completed

---
+++ hello.py
@@ -0,0 +1,15 @@
+print(2+3)
+print('Hello')
+print(100)
+print("Hey there!")
+#Homework: Learn how to change comment syntax in python
+x = 100
+a = "hello"
+b = 5.43
+print(x,a,b)
+#printing data type of var.
+type(a)
+print(type(a))
+sum1=x+b
+mul1=x*b
+print(sum1,mul1)
---
+++ task1.py
@@ -0,0 +1,5 @@
+#by default input  takes all data as string data type
+num1 = int(input('Enter number 1: '))
+num2 = int(input('Enter number 2: '))
+sum = num1+num2
+print("The summation of numbers 1 & 2 is ",sum)
